672f8096a5c7a0af009189ea8960b1473f9541ee
ssantisteban <sergios@mediprocesos.com>
2023-06-02 10:06:39 -0600

    fist changes

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Sergio Santisteban, a Full Stack Developer .';
+
+  static const String aboutMeDetail =
+      'I\'m Sergio Santisteban, a Full Stack Developer with over 8 years of experience as web & mobile developer. Currently my area of ​​expertise is in the development of mobile and web applications using the Flutter tool.';
+}
---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,57 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/snackbar.png",
+    "assets/projects/quranB.png",
+    "assets/projects/medkitB.png",
+    "assets/projects/hereiamB.png",
+    "assets/projects/covidB.png",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/flutter.png",
+    "assets/projects/quran.png",
+    "assets/projects/medkit.png",
+    "assets/projects/hereiam.png",
+    "assets/projects/covid.png",
+    "assets/projects/messenger.png",
+    "assets/projects/flutter.png",
+    "assets/projects/earbender.png",
+    "assets/projects/java.png",
+    "assets/projects/android.png",
+    "assets/services/open_b.png",
+  ];
+
+  static const List<String> titles = [
+    "Elementary",
+    "RPN",
+    "Roble Red",
+    "GMFicohsa",
+    "Doctor Plus",
+    "Assa salud",
+    "e-com",
+  ];
+
+  static const List<String> description = [
+    "A collection of modules used for fast development of mobile apps using a defined arquitecture.",
+    "App of medic insure based on elementary modules, contains providers search with mongoDB, integrated with firebase and geolocalization of closest providers.",
+    "App based on providers module of elementary with unique functuniality of cupons based on the plan of the insured.",
+    "App with firebase, mongoDB with a option of uploading and create pdf and generate authorizations for medicaments and appoinments.",
+    "e-commerce platform to schedule deliver of medicaments and medical appoinments.",
+    "App based on elementary module with a custom login process based on country and policies of the companys.",
+    "Personal e-commerce platform with admin panel, reports, and user friendly personalization for white labeling.",
+  ];
+
+  static const List<String> links = [
+    "https://pub.dev/packages/awesome_snackbar_content",
+    "https://github.com/mhmzdev/The_Holy_Quran_App",
+    "https://github.com/mhmzdev/MedKit-Pharmacy-App-Using-Flutter",
+    "https://github.com/mhmzdev/Here-I-Am-Alert-App",
+    "https://github.com/mhmzdev/Covid19-Tracker-App",
+    "https://github.com/mhmzdev/Messenger-Chat-Head-Flutter-UI",
+    "https://github.com/mhmzdev/flutter.dev-Flutter-Web-Clone",
+    "https://github.com/mhmzdev/Earbender_Music_App",
+    "https://github.com/mhmzdev/FTP_GUI_Java",
+    "https://github.com/mhmzdev/My-Order-Resturant-Ordering-System",
+    "https://github.com/mhmzdev/Hidev_Web",
+  ];
+}
